383e982555a4b2dc25ed69050ec14e09402ea26a
Neomaster <thbangkok@users.noreply.github.com>
2023-06-30 23:07:57 +0700

    DD-4 Add TemplateName to the Project
    Co-authored-by: NAPHONG SUDTHISORNYOTHIN <naphongs.scbam@scb.co.th>

--- DevDynamo.Web/Areas/ApiV1/Controllers/ProjectsController.cs
+++ DevDynamo.Web/Areas/ApiV1/Controllers/ProjectsController.cs
@@ -6,56 +6,58 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DevDynamo.Web.Areas.ApiV1.Controllers
 {
-  [Route("api/v1/[controller]")]
-  [ApiController]
-  public class ProjectsController : ControllerBase
-  {
-    private readonly AppDb db;
-
-    public ProjectsController(AppDb db)
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
     {
-      this.db = db;
+        private readonly AppDb db;
+
+        public ProjectsController(AppDb db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ProjectResponse>> GetAll()
+        {
+            var items = db.Projects.ToList();
+            return items.ConvertAll(x => ProjectResponse.FromModel(x));
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ProjectResponse> GetById(Guid id)
+        {
+            var item = db.Projects.SingleOrDefault(x => x.Id == id);
+            if (item is null)
+            {
+                //return NotFound("Project not found");
+                return NotFound(new ProblemDetails() { Title = "Project is not found" });
+            }
+            return ProjectResponse.FromModel(item);
+        }
+
+        [HttpPost]
+        public ActionResult<ProjectResponse> Create(CreateProjectRequest request)
+        {
+            var p = new Project(request.Name);
+
+            var path = $"./WorkflowTemplates/{request.Template}.txt";
+            if (!System.IO.File.Exists(path))
+            {
+                return BadRequest(new ProblemDetails { Title = $"Template {request.Template} not found" });
+            }
+
+            var workflow = System.IO.File.ReadAllText(path);
+            p.LoadWorkflowTemplate(workflow);
+
+            p.TemplateName = request.Template;
+
+            db.Projects.Add(p);
+            db.SaveChanges();
+
+            var res = ProjectResponse.FromModel(p);
+            return CreatedAtAction(nameof(GetById), new { id = p.Id }, res);
+        }
+
     }
-
-    [HttpGet]
-    public ActionResult<IEnumerable<ProjectResponse>> GetAll()
-    {
-      var items = db.Projects.ToList();
-      return items.ConvertAll(x => ProjectResponse.FromModel(x));
-    }
-
-    [HttpGet("{id}")]
-    public ActionResult<ProjectResponse> GetById(Guid id)
-    {
-      var item = db.Projects.SingleOrDefault(x => x.Id == id);
-      if (item is null)
-      {
-        //return NotFound("Project not found");
-        return NotFound(new ProblemDetails() { Title = "Project is not found" });
-      }
-      return ProjectResponse.FromModel(item);
-    }
-
-    [HttpPost]
-    public ActionResult<ProjectResponse> Create(CreateProjectRequest request)
-    {
-      var p = new Project(request.Name);
-
-      var path = $"./WorkflowTemplates/{request.Template}.txt";
-      if (!System.IO.File.Exists(path))
-      {
-        return BadRequest(new ProblemDetails { Title = $"Template {request.Template} not found" });
-      }
-
-      var workflow = System.IO.File.ReadAllText(path);
-      p.LoadWorkflowTemplate(workflow);
-
-      db.Projects.Add(p);
-      db.SaveChanges();
-
-      var res = ProjectResponse.FromModel(p);
-      return CreatedAtAction(nameof(GetById), new { id = p.Id }, res);
-    }
-
-  }
 }
