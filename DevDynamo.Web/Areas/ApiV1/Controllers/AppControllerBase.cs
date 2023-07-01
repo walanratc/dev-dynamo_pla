5685fc58e6901ce61d30ae1c663d4b5514c18d87
Walanrat Chaichong <Walanratc@scbam.onmicrosoft.com>
2023-07-01 11:59:27 +0700

    DD-001
    change appsetting

---
+++ DevDynamo.Web/Areas/ApiV1/Controllers/AppControllerBase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace DevDynamo.Web.Areas.ApiV1.Controllers
+{
+    public abstract class AppControllerBase : ControllerBase
+    {
+        public NotFoundObjectResult AppNotFound(string objectName,object? keyThatNotFound = null, string message="")
+        {   
+            var s = $"{objectName} was not found.";
+
+            if (keyThatNotFound != null)
+                s += $" {keyThatNotFound}";
+            if (message != null)
+                s += $" {message}";
+
+            var obj = new ProblemDetails
+            {
+                Status = (int)HttpStatusCode.NotFound,
+                Title = s
+            };
+            return base.NotFound(obj);
+        }
+    }
+}
