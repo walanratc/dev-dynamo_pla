a9af3cc653a6534fd2935a41d6aee1b362c6a246
Suthep Sangvirotjanaphat <suthep@gfbd.co.th>
2023-07-01 14:09:07 +0700

    Enable EF lazy loading

--- DevDynamo.Web/Program.cs
+++ DevDynamo.Web/Program.cs
@@ -4,69 +4,71 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DevDynamo.Web
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    public class Program
     {
-      var builder = WebApplication.CreateBuilder(args);
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-      // Add services to the container.
-      var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-      Console.WriteLine(nameof(AppDb));
-      Console.WriteLine(builder.Configuration.GetConnectionString(nameof(AppDb)));
+            Console.WriteLine(nameof(AppDb));
+            Console.WriteLine(builder.Environment.EnvironmentName);
+            Console.WriteLine(builder.Configuration.GetConnectionString("AppDb"));
 
-      builder.Services.AddDbContext<ApplicationDbContext>(options =>
-          options.UseSqlServer(connectionString));
-      builder.Services.AddDbContext<AppDb>(options =>
-      {
-        options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb"));
-      });
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            builder.Services.AddDbContext<AppDb>(options =>
+            {
+                options.UseLazyLoadingProxies()
+                    .UseSqlServer(builder.Configuration.GetConnectionString("AppDb"));
+            });
 
-      builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-      builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-          .AddEntityFrameworkStores<ApplicationDbContext>();
-      builder.Services.AddControllersWithViews();
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddControllersWithViews();
 
-      builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen();
 
-      var app = builder.Build();
+            var app = builder.Build();
 
-      // Configure the HTTP request pipeline.
-      if (app.Environment.IsDevelopment())
-      {
-        app.UseMigrationsEndPoint();
-        app.UseSwagger();
-        app.UseSwaggerUI();
-      }
-      else
-      {
-        app.UseExceptionHandler("/Home/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-      }
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
-      app.UseHttpsRedirection();
-      app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-      app.UseRouting();
+            app.UseRouting();
 
-      app.UseAuthentication();
-      app.UseAuthorization();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
 
-      //app.MapControllerRoute(
-      //    name: "ApiV1",
-      //    pattern: "api/v1/{controller=Home}/{action=Index}/{id?}", new { Area = "ApiV1" });
+            //app.MapControllerRoute(
+            //    name: "ApiV1",
+            //    pattern: "api/v1/{controller=Home}/{action=Index}/{id?}", new { Area = "ApiV1" });
 
-      app.MapControllerRoute(
-          name: "default",
-          pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
-      app.MapRazorPages();
+            app.MapRazorPages();
 
-      app.Run();
+            app.Run();
+        }
     }
-  }
 }
